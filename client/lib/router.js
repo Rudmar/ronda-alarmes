cdba5eee165f1590cce5eff45af691c9d2683e57
Rudmar <rudmar_junior@hotmail.com>
2018-01-10 03:35:18 -0200

    Exemplo de postagem

--- client/lib/router.js
+++ client/lib/router.js
@@ -2,3 +2,6 @@ Router.route('home', {path: '/'}); // Add this route
 Router.route('solucoes_Inteligentes', function () {
   this.render('solucoesInteligentes');
 });
+Router.route('exemplo', function () {
+  this.render('exemplo_postagem');
+});
