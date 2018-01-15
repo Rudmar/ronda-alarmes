1621df936293a65b13bc085930416b2bf5f34a4a
Rudmar <rudmar_junior@hotmail.com>
2018-01-15 02:48:49 -0200

    Sitemap.xml

--- server/sitemaps.js
+++
@@ -1,9 +0,0 @@
-sitemaps.add('/sitemap.xml', function() {
-  // required: page
-  // optional: lastmod, changefreq, priority, xhtmlLinks, images, videos
-  return [
-    { page: '/home', lastmod: new Date().getTime(), changefreq: 'monthly', priority: 0.8 },
-    // https://support.google.com/webmasters/answer/178636?hl=en
-    { page: '/solucoes_Inteligentes', lastmod: new Date().getTime(), changefreq: 'monthly', priority: 0.5 },
-  ];
-});
