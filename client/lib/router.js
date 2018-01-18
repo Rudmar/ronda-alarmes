61994b3aef1567f314c8a3cb0d189ad2ef0a2e68
Rudmar <rudmar_junior@hotmail.com>
2018-01-18 14:20:11 -0200

    Artigo 2

--- client/lib/router.js
+++ client/lib/router.js
@@ -2,9 +2,6 @@ Router.route('home', {path: '/home'}); // Add this route
 Router.route('solucoes_Inteligentes', function () {
   this.render('solucoesInteligentes');
 });
-Router.route('solucoes_Inteligentes/', function () {
-  this.render('solucoesInteligentes');
-});
 // Primeiro se escolhe o nome que vai aparecer no link do site, depois o nome do template a ser usado
 Router.route('exemplo', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
@@ -17,8 +14,7 @@ Router.route('seguranca_para_empresas', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
   this.render('post1');
 });
-
-Router.route('seguranca_para_empresas/', function () {
+Router.route('principais_elementos_para_escolha_de_empresa_de_seguranca', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
-  this.render('post1');
+  this.render('post2');
 });
