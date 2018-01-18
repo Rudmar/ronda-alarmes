b323cfe0f74c2b7729c7396f43afcca1053c683e
Rudmar <rudmar_junior@hotmail.com>
2018-01-18 13:19:42 -0200

    Route fix

--- client/lib/router.js
+++ client/lib/router.js
@@ -2,6 +2,9 @@ Router.route('home', {path: '/home'}); // Add this route
 Router.route('solucoes_Inteligentes', function () {
   this.render('solucoesInteligentes');
 });
+Router.route('solucoes_Inteligentes/', function () {
+  this.render('solucoesInteligentes');
+});
 // Primeiro se escolhe o nome que vai aparecer no link do site, depois o nome do template a ser usado
 Router.route('exemplo', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
@@ -11,6 +14,11 @@ Router.route('/', function () {
   this.render('home');
 });
 Router.route('seguranca_para_empresas', function () {
+  // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
+  this.render('post1');
+});
+
+Router.route('seguranca_para_empresas/', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
   this.render('post1');
 });
