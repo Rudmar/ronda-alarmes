1e55048d411df974fbb6371f290e98e43f24e05f
Rudmar <rudmar_junior@hotmail.com>
2018-01-30 14:52:44 -0200

    Artigo 3

--- client/lib/router.js
+++ client/lib/router.js
@@ -3,10 +3,6 @@ Router.route('solucoes_Inteligentes', function () {
   this.render('solucoesInteligentes');
 });
 // Primeiro se escolhe o nome que vai aparecer no link do site, depois o nome do template a ser usado
-Router.route('exemplo', function () {
-  // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
-  this.render('exemplo_postagem');
-});
 Router.route('/', function () {
   this.render('home');
 });
@@ -17,4 +13,12 @@ Router.route('seguranca_para_empresas', function () {
 Router.route('principais_elementos_para_escolha_de_empresa_de_seguranca', function () {
   // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
   this.render('post2');
+});
+Router.route('porque_contratar_uma_empresa_de_seguranca', function () {
+  // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
+  this.render('post3');
+});
+Router.route('postagens', function () {
+  // Aqui vai o nome do template do site, nome dado no parametro <template name="exemplo_postagem">
+  this.render('todos_posts');
 });
